1c959d5538b1595a8bbcabd4b3784928d28a5917
Doug Grainger <dhgrainger@gmail.com>
2014-11-16 02:58:33 -0500

    nav bar done

--- js/parallax.js
+++ js/parallax.js
@@ -9,6 +9,8 @@ window.requestAnimationFrame = window.requestAnimationFrame
 var image = document.getElementById('image')
 var title = document.getElementById('title')
 var nav = document.getElementById('nav')
+var par = document.getElementById('par')
+var pic = document.getElementById('pic')
 
 var scrollheight = document.body.scrollHeight // height of entire document
 var windowheight = window.innerHeight // height of browser window
@@ -16,14 +18,21 @@ var windowheight = window.innerHeight // height of browser window
 function parallaxnav(){
  var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
  image.style.bottom = scrolltop * 3 + 'px' // move bubble2 at 50% of scroll rate
+
  nav.style.opacity = scrolltop / 600
+ par.style.opacity = 10 / scrolltop
+
+
  if(scrolltop > 150){
    title.style.opacity = 0
  }
  else{
   title.style.opacity = 1
  }
-
+ pic.style.left = (15 - (scrolltop / 20)) + '%'
+ pic.style.height = (66 - (scrolltop/ 5)) + '%'
+ // pic.style.width = (30 - (scrolltop/ 40)) + '%'
+ pic.style.bottom = (15 - (scrolltop/ 20)) + '%'
 }
 
 window.addEventListener('scroll', function(){ // on page scroll
