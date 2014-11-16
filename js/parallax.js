357b080bc76cbdc6b3925d309ca20d463ec17949
Doug Grainger <dhgrainger@gmail.com>
2014-11-16 00:23:49 -0500

    this is the one

--- js/parallax.js
+++ js/parallax.js
@@ -16,9 +16,9 @@ var windowheight = window.innerHeight // height of browser window
 function parallaxbubbles(){
  var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
  navy.style.top = scrolltop * 1 + 'px' // move bubble1 at 20% of scroll rate
- image.style.top = scrolltop * 3 + 'px' // move bubble2 at 50% of scroll rate
+ image.style.bottom = scrolltop * 3 + 'px' // move bubble2 at 50% of scroll rate
  navy.style.opacity = scrolltop / 384
- image.style.opacity = scrolltop / 384
+ // image.style.opacity = scrolltop / 384
 }
 
 window.addEventListener('scroll', function(){ // on page scroll
