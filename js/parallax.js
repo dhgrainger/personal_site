840b3ce5cbd59bec65b32ae5a1f2ffa0636c29b8
Doug Grainger <dhgrainger@gmail.com>
2014-11-24 23:37:24 -0500

    refactored js and made a name change:

--- js/parallax.js
+++ js/parallax.js
@@ -1,34 +1,41 @@
-window.requestAnimationFrame = window.requestAnimationFrame
- || window.mozRequestAnimationFrame
- || window.webkitRequestAnimationFrame
- || window.msRequestAnimationFrame
- || function(f){setTimeout(f, 1000/60)}
+// window.requestAnimationFrame = window.requestAnimationFrame
+//  || window.mozRequestAnimationFrame
+//  || window.webkitRequestAnimationFrame
+//  || window.msRequestAnimationFrame
+//  || function(f){setTimeout(f, 1000/60)}
 
 var image = document.getElementById('image')
 var title = document.getElementById('title')
 var nav = document.getElementById('nav')
 var par = document.getElementById('par')
-var pic = document.getElementById('pic')
 
-function parallaxnav(){
- var scrolltop = window.pageYOffset
- image.style.bottom = scrolltop * 3 + 'px'
+function parallaxNav(scrolltop){
+ navDiv.style.bottom = scrolltop * 3 + 'px'
  nav.style.opacity = scrolltop / 600
  par.style.opacity = 1 - (scrolltop / 100 )
- if(scrolltop > 150){
-   title.style.opacity = 0
- }
- else{
-  title.style.opacity = 1
- }
- pic.style.left = (15 - (scrolltop / 20)) + '%'
- pic.style.height = (66 - (scrolltop/ 5)) + '%'
- // pic.style.width = (30 - (scrolltop/ 40)) + '%'
- pic.style.bottom = (15 - (scrolltop/ 20)) + '%'
+
+}
+
+function titleStyle(scrolltop){
+  if(scrolltop > 150){
+    title.style.opacity = 0
+  }
+  else{
+   title.style.opacity = 1
+  }
+}
+function pictureMove(scrolltop){
+  var pic = document.getElementById('pic')
+  pic.style.left = (15 - (scrolltop / 20)) + '%'
+  pic.style.height = (66 - (scrolltop/ 5)) + '%'
+  pic.style.bottom = (15 - (scrolltop/ 20)) + '%'
 }
 
 window.addEventListener('scroll', function(){
- requestAnimationFrame(parallaxnav)
+  var scrolltop = window.pageYOffset
+  parallaxNav(scrolltop)
+  pictureMove(scrolltop)
+  titleStyle(scrolltop)
 }, false)
 
 
